e55f82c0577f5bd753153e3e321b8e017524b579
dpokolo <pokolodnyydmytro@gmail.com>
2025-11-12 09:12:01 -0500

    change main to read file

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(path_to_file):
+    file_content = ""
+    with open(path_to_file) as f:
+        file_content = f.read()
+    return file_content
+
+def main():
+    get_book_text("books/frankenstein.txt")
+
+main()
